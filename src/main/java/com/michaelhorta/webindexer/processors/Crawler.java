1f469340381afd3245bdb39ec1ba24eada877f7b
Michael Horta Fleitas <michaelhorta32@gmail.com>
2017-10-24 14:48:57 -0400

    Add method execute which crawl an url with Jsoup package

--- src/main/java/com/michaelhorta/webindexer/processors/Crawler.java
+++ src/main/java/com/michaelhorta/webindexer/processors/Crawler.java
@@ -1,6 +1,52 @@
 package com.michaelhorta.webindexer.processors;
 
+import com.michaelhorta.webindexer.beans.Page;
+import com.michaelhorta.webindexer.utils.Url;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Crawler {
 
+    private String url;
 
+    public Crawler(String url) {
+        this.url = url;
+    }
+
+    public Page execute() {
+        try {
+            Document document = Jsoup.connect(this.url).get();
+
+            String location = document.location();
+
+            String title = document.title();
+            String s = document.ownText();
+            String content = document.body().text();
+            Elements outlinkElements = document.select("a[href]");
+            Iterator<Element> elementIterator = outlinkElements.iterator();
+            ArrayList<String> outlinks = new ArrayList<>();
+            while(elementIterator.hasNext()) {
+                String outlinkUrl = Url.removeLastSlash(elementIterator.next().attr("href"));
+                outlinks.add(outlinkUrl);
+            }
+            return new Page(this.url, title, content, outlinks);
+        } catch (Exception e) {
+//            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
