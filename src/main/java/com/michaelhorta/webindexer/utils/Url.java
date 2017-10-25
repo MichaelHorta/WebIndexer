dcdc9e44aaba062fc6ac031c9e353dbe81de4412
Michael Horta Fleitas <michaelhorta32@gmail.com>
2017-10-25 00:57:35 -0400

    Add checking isUrl

--- src/main/java/com/michaelhorta/webindexer/utils/Url.java
+++ src/main/java/com/michaelhorta/webindexer/utils/Url.java
@@ -1,5 +1,8 @@
 package com.michaelhorta.webindexer.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Url {
     private static Url ourInstance = new Url();
 
@@ -10,7 +13,17 @@ public class Url {
     private Url() {
     }
 
+    public static boolean isUrl(String url) {
+
+        if (url.length() > 4 && url.substring(0, 4).equals("http")) {
+            return true;
+        }
+
+        return false;
+    }
+
     public static String removeLastSlash(String url) {
+        System.out.println(url);
         if (url.charAt(url.length() - 1) == '/')
             return url.substring(0, url.length() - 1);
 
