65c5bce8fd499559324c984ab8aaa2e38adf9e0a
Michael Horta Fleitas <michaelhorta32@gmail.com>
2017-10-25 00:05:20 -0400

    Add abstract methods:    - executeClearer    - executeSearcher

--- src/main/java/com/michaelhorta/webindexer/service/IManagerService.java
+++ src/main/java/com/michaelhorta/webindexer/service/IManagerService.java
@@ -1,6 +1,7 @@
 package com.michaelhorta.webindexer.service;
 
 import com.michaelhorta.webindexer.beans.IndexerResponse;
+import com.michaelhorta.webindexer.beans.SearcherResponse;
 import org.apache.lucene.queryparser.classic.ParseException;
 
 import java.io.IOException;
@@ -8,4 +9,8 @@ import java.io.IOException;
 public interface IManagerService {
 
     IndexerResponse executeIndexer(String url) throws IOException, ParseException;
+
+    boolean executeClearer(String url) throws IOException, ParseException;
+
+    SearcherResponse executeSearcher(String word) throws IOException, ParseException;
 }
